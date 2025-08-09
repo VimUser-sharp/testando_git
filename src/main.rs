fea2de867682cfce3536cbb3564f02b89145cb6f
VimUser-sharp <fronterlinemail@tutamail.com>
2025-08-08 23:48:38 -0300

    adicionado print indicativo na UI do console

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use crate::scope::logic;
-use std::io;
+use std::io::{self, Write};
 
 mod scope;
 
 fn main() {
-    let mut name = String::new();
-
-    //print!("Digite seu nome: ");
+    print!("Digite seu nome: ");
+    io::stdout().flush().unwrap();
+    let mut name: String = String::new();
     io::stdin().read_line(&mut name).expect("falha na entrada");
     logic(&name);
 }
