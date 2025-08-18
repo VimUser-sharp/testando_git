0feaa1adf9351890bcd2a7cc4f46bca62d407eec
VimUser-sharp <fronterlinemail@tutamail.com>
2025-08-17 21:23:06 -0300

    struct Person adicionada e usada

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,40 @@ use std::io::{self, Write};
 mod clean_screen;
 mod scope;
 
+struct Person {
+    name: String,
+    age: u32,
+    height: f32,
+}
+
 fn main() {
-    let mut name: String = String::new();
+    let mut people: Person = Person {
+        name: String::new(),
+        age: 0,
+        height: 0.0,
+    };
+    let mut input: String = String::new();
 
     print!("Digite seu nome: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut name).expect("falha na entrada");
+    io::stdin()
+        .read_line(&mut people.name)
+        .expect("falha na entrada");
     clear();
 
-    logic(&name);
+    print!("Digite sua idade: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).expect("falha na entrada");
+    clear();
+
+    people.age = input.trim().parse().expect("erro ao converter para int");
+
+    print!("Digite sua altura: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).expect("falha na entrada");
+    clear();
+
+    people.height = input.trim().parse::<f32>().unwrap_or(0.00);
+
+    logic(&people.name);
 }
