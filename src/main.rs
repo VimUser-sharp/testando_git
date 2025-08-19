c10968db9f1ef5e00a43e65e42af3a9487bbf8ad
VimUser-sharp <fronterlinemail@tutamail.com>
2025-08-18 22:39:35 -0300

    struct adicionada

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,40 @@ use std::io::{self, Write};
 mod clean_screen;
 mod scope;
 
+struct Person {
+    name: String,
+    age: u32,
+}
+
 fn main() {
-    let mut name: String = String::new();
+    let mut people: Person = Person {
+        name: String::new(),
+        age: 0,
+    };
+    let mut input: String = String::new();
 
     print!("Digite seu nome: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut name).expect("falha na entrada");
+    io::stdin()
+        .read_line(&mut people.name)
+        .expect("falha na entrada");
     clear();
 
-    let p_name = &mut name;
+    print!("Digite sua idade: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).expect("falha na entrada");
+    clear();
+
+    let mut p_name = &mut people.name;
     *p_name = String::from("cleiton");
 
-    logic(&name);
+    p_name = &mut input;
+    *p_name = String::from("99");
+
+    people.age = input
+        .trim()
+        .parse()
+        .expect("erro ao converter para inteiro");
+
+    logic(&people);
 }
